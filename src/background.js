8e34228b88c600348ffcc1839786ecee280698f2
Victor Aro <varo@eusou.space>
2021-05-19 01:40:14 -0300

    Fixed launcher freeze

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+// import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -44,13 +44,15 @@ async function createWindow() {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    splash.loadURL('app://./splash.html')
     win.loadURL('app://./index.html')
+    splash.loadURL('app://./splash.html')
   }
   win.once('ready-to-show', () => {
     setTimeout(function () {
       splash.close();
-      win.show();
+      setTimeout(function () {
+        win.show();
+      }, 300);
     }, 2000);
   })
 }
@@ -74,14 +76,7 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
-  }
+
   createWindow()
 })
 
